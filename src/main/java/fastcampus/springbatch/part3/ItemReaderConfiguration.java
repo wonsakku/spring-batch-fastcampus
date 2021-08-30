1fa9f29b0f2c126733974996b0419322f464b89b
kws1044 <kws1044@gmail.com>
2021-08-30 17:45:53 +0900

    CSV 파일 데이터 읽기

--- src/main/java/fastcampus/springbatch/part3/ItemReaderConfiguration.java
+++ src/main/java/fastcampus/springbatch/part3/ItemReaderConfiguration.java
@@ -10,8 +10,13 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,10 +30,11 @@ public class ItemReaderConfiguration {
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job itemReaderJob() {
+	public Job itemReaderJob() throws Exception{
 		return jobBuilderFactory.get("itemReaderJob")
 				.incrementer(new RunIdIncrementer())
 				.start(this.customItemReaderStep())
+				.next(this.csvFileStep())
 				.build();
 	}
 
@@ -40,6 +46,51 @@ public class ItemReaderConfiguration {
 //				.processor()
 				.writer(itemWriter())
 				.build();
+	}
+	
+	@Bean
+	public Step csvFileStep() throws Exception{
+		return stepBuilderFactory.get("csvFileStep")
+				.<Person, Person>chunk(5)
+				.reader(this.csvFileItemReader())
+				.writer(itemWriter())
+				.build();
+	}
+	
+	private FlatFileItemReader<Person> csvFileItemReader() throws Exception{
+//		data를 읽을 수 있는 설정
+//		csv파일을 1줄씩 읽을 수 있는 설정
+		DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+		
+//		csv 파일을 Person 객체에 매핑하기 위해 Person 필드명을 설정하는 Tokenizer 객체 필요
+		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+		tokenizer.setNames("id", "name", "age", "address");
+		
+		lineMapper.setLineTokenizer(tokenizer);
+		
+		lineMapper.setFieldSetMapper(fieldSet -> {
+			int id = fieldSet.readInt("id");
+			String name = fieldSet.readString("name");
+			String age = fieldSet.readString("age");
+			String address = fieldSet.readString("address");
+			
+			return new Person(id, name, age, address);
+		});
+		
+		
+		FlatFileItemReader<Person> itemReader = new FlatFileItemReaderBuilder<Person>()
+												.name("csvFileReader")
+												.encoding("UTF-8")
+												.resource(new ClassPathResource("test.csv"))
+												.linesToSkip(1)
+												.lineMapper(lineMapper)
+												.build()
+												;
+		
+		itemReader.afterPropertiesSet();
+		
+		return itemReader;
+		
 	}
 
 	private ItemWriter<? super Person> itemWriter() {
