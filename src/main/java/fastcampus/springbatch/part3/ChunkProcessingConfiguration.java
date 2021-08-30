6e883e6cb1e525a56d56b597c06bd0cdbc3c2e9d
kws1044 <kws1044@gmail.com>
2021-08-30 16:05:52 +0900

    @JobScope와 @StepScope 이해

--- src/main/java/fastcampus/springbatch/part3/ChunkProcessingConfiguration.java
+++ src/main/java/fastcampus/springbatch/part3/ChunkProcessingConfiguration.java
@@ -3,12 +3,14 @@ package fastcampus.springbatch.part3;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
@@ -16,9 +18,13 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,14 +41,15 @@ public class ChunkProcessingConfiguration {
 		return jobBuilderFactory.get("chunkProcessingJob")
 				.incrementer(new RunIdIncrementer())
 				.start(this.taskBaseStep())
-				.next(this.chunkBaseStep())
+//				.next(this.chunkBaseStep())
+				.next(this.chunkBaseStep(null))
 				.build();
 	}
 
 	@Bean
 	public Step taskBaseStep() {
 		return stepBuilderFactory.get("taskBaseStep")
-				.tasklet(this.tasklet())
+				.tasklet(this.tasklet(null))
 				.build()
 				;
 	}
@@ -50,16 +57,18 @@ public class ChunkProcessingConfiguration {
 	
 
 	@Bean
-	public Step chunkBaseStep() {
+	@JobScope
+//	@Scope(value = "job", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public Step chunkBaseStep(@Value("#{jobParameters[chunkSize]}") String chunkSize) {
 		return stepBuilderFactory.get("chunkBaseStep")
-				.<String, String>chunk(24)
+//				.<String, String>chunk(24)
+				.<String, String>chunk(StringUtils.isNotEmpty(chunkSize) ? Integer.parseInt(chunkSize) : 10) 
 				.reader(itemReader())
 				.processor(itemProcessor())
 				.writer(itemWriter())
 				.build()
 				;
 	}
-	
 	
 	
 	private ItemWriter<? super String> itemWriter() {
@@ -75,14 +84,21 @@ public class ChunkProcessingConfiguration {
 		return new ListItemReader<>(getItems());
 	}
 
-	private Tasklet tasklet() {
+
+	@Bean
+	@StepScope
+//	public Tasklet tasklet() {
+	public Tasklet tasklet(@Value("#{jobParameters[chunkSize]}") String value) {
 		List<String> items = getItems();
 		
 		return (contribution, chunkContext) -> {
 			
 			StepExecution stepExecution = contribution.getStepExecution();
+//			JobParameters jobParameters = stepExecution.getJobParameters();
 			
-			int chunkSize = 14;
+			
+//			String value = jobParameters.getString("chunkSize", "10");
+			int chunkSize = StringUtils.isNotEmpty(value) ? Integer.parseInt(value) : 10;
 			int fromIndex = stepExecution.getReadCount();
 			int toIndex = fromIndex + chunkSize;
 			
