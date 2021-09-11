e5bcd05e9081ad6b7620bc0bc444786820c9c2ae
Kim Won Sik <kws1044@gmail.com>
2021-09-11 10:50:50 +0900

    retry 예외처리 Done

--- src/main/java/fastcampus/springbatch/part3/PersonValidationRetryProcessor.java
+++ src/main/java/fastcampus/springbatch/part3/PersonValidationRetryProcessor.java
@@ -1,9 +1,15 @@
 package fastcampus.springbatch.part3;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryListener;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.retry.support.RetryTemplateBuilder;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PersonValidationRetryProcessor implements ItemProcessor<Person, Person>{
 
 	private final RetryTemplate retryTemplate;
@@ -13,6 +19,7 @@ public class PersonValidationRetryProcessor implements ItemProcessor<Person, Per
 		this.retryTemplate = new RetryTemplateBuilder()
 				.maxAttempts(3)
 				.retryOn(NotFoundNameException.class)
+				.withListener(new SavePersonRetryListener())
 				.build();
 	}
 
@@ -27,13 +34,38 @@ public class PersonValidationRetryProcessor implements ItemProcessor<Person, Per
 				return item;
 			}
 			
-			throw new NotFoundNameException();
 			
+			log.info("RetryCallback");
+			throw new NotFoundNameException();
 		}, context -> {
 			// RecoveryCallback
+			log.info("RetryCallback");
 			return item.unknownName();
 		});
 	}
+	
+	//org.springframework.retry.RetryListener
+	public static class SavePersonRetryListener implements RetryListener{
+
+		@Override // retry를 시작하는 설정; return true여야 retry가 적용
+		public <T, E extends Throwable> boolean open(RetryContext context, RetryCallback<T, E> callback) {
+			return true;
+		}
+
+		@Override // retry 종료 후에 호출. 
+		public <T, E extends Throwable> void close(RetryContext context, RetryCallback<T, E> callback,
+				Throwable throwable) {
+			log.info("close");
+		}
+
+		@Override // retry Template에 정의한 exception(NotFoundNameException)이 발생했을 때 호출.
+		public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback,
+				Throwable throwable) {
+			log.info("onError");
+		}
+		
+	}
+	
 }
 
 
